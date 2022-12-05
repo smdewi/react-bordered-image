bcf61fc121d3d2af17195bd45d26137ac73f1042
smdewi <smdewi2020@gmail.com>
2022-12-05 15:04:58 +0800

    Fix img src format

--- src/BorderedImageFrame.js
+++ src/BorderedImageFrame.js
@@ -1,7 +1,7 @@
 function BorderedImageFrame() {
     return (
         <div className="border-img">
-            <img src={require("./gingerbread-town.jpeg")} alt={"Gingerbread Town in Bergen, Norway"} />
+            <img src="gingerbread-town.jpeg" alt="Gingerbread Town in Bergen, Norway" />
             <p>Gingerbread Town in Bergen, Norway</p>
         </div>
         
